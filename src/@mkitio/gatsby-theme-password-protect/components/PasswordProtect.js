ffbc7d0006130525ae312a138a43bb3d53d40fb6
ryanwashburne <ryan.washburne@gmail.com>
2020-09-03 16:28:26 -0700

    fix

--- src/@mkitio/gatsby-theme-password-protect/components/PasswordProtect.js
+++ src/@mkitio/gatsby-theme-password-protect/components/PasswordProtect.js
@@ -4,6 +4,8 @@ import {
   getSessionPassword,
 } from '@mkitio/gatsby-theme-password-protect/src/utils/utils'
 
+import SEO from '../../../components/seo'
+
 export default () => {
   const initial = getSessionPassword()
   const [input, setInput] = useState(initial || '')
@@ -13,25 +15,28 @@ export default () => {
     window.location.reload()
   }
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      <form onSubmit={handleSubmit} className="w-1/2 lg:w-1/4">
-        <input
-          className="form-input w-full mb-4"
-          onChange={(e) => setInput(e.target.value)}
-          value={input}
-          placeholder="Password"
-          autoFocus
-        />
-        <button
-          className="text-lg px-3 py-1 rounded hover:bg-gray-200 hover:shadow border"
-          type="submit"
-        >
-          Submit
-        </button>
-        <p className={`text-red-500 mt-4 ${initial ? '' : 'invisible'}`}>
-          Password incorrect.
-        </p>
-      </form>
+    <div>
+      <div className="min-h-screen flex flex-col justify-center items-center">
+        <SEO />
+        <form onSubmit={handleSubmit} className="container mx-auto lg:w-1/3">
+          <input
+            className="form-input w-full mb-4"
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+            placeholder="Password"
+            autoFocus
+          />
+          <button
+            className="text-lg px-3 py-1 rounded hover:bg-gray-200 hover:shadow border"
+            type="submit"
+          >
+            Submit
+          </button>
+          <p className={`text-red-500 mt-4 ${initial ? '' : 'invisible'}`}>
+            Password incorrect.
+          </p>
+        </form>
+      </div>
     </div>
   )
 }
