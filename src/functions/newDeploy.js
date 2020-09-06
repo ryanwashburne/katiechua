3edb1e70f511a1bf78efdca4bc0be9dd00d351d4
ryanwashburne <ryan.washburne@gmail.com>
2020-09-05 19:22:09 -0700

    added dep

--- src/functions/newDeploy.js
+++ src/functions/newDeploy.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch').default
+// const fetch = require('node-fetch').default
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
 exports.handler = async (event, context) => {
